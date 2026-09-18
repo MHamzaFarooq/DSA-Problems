0be6eb31030c3d0f236456dc259866ca64361ccd
Hamza Farooq <hamzafarooq49ml@gmail.com>
2026-09-18 17:00:53 +0500

    solved hash maps questions

---
+++ arrays/majorityElement.js
@@ -0,0 +1,32 @@
+var majorityElement = function (nums) {
+  const hash = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (hash.has(nums[i])) {
+      hash.set(nums[i], hash.get(nums[i]) + 1);
+    } else {
+      hash.set(nums[i], 1);
+    }
+    if (hash.get(nums[i]) > nums.length / 2) {
+      return nums[i];
+    }
+  }
+};
+
+// The map overhead makes this solution slower than the Boyer-Moore Voting Algorithm, but it is still O(n) time complexity and O(n) space complexity.
+//a simpler solutions lies in simple arithmetic
+
+var majorityElement = function (nums) {
+  const hash = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (hash.has(nums[i])) {
+      hash.set(nums[i], hash.get(nums[i]) + 1);
+    } else {
+      hash.set(nums[i], 1);
+    }
+    if (hash.get(nums[i]) > nums.length / 2) {
+      return nums[i];
+    }
+  }
+};
