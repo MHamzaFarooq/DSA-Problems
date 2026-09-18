38cc61b650a048cca082fd454a248af204031425
Hamza Farooq <hamzafarooq49ml@gmail.com>
2026-09-18 17:02:49 +0500

    solved hash maps questions

--- arrays/majorityElement.js
+++ arrays/majorityElement.js
@@ -17,16 +17,13 @@ var majorityElement = function (nums) {
 //a simpler solutions lies in simple arithmetic
 
 var majorityElement = function (nums) {
-  const hash = new Map();
+  let count = 0;
+  let candidate = null;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (hash.has(nums[i])) {
-      hash.set(nums[i], hash.get(nums[i]) + 1);
-    } else {
-      hash.set(nums[i], 1);
-    }
-    if (hash.get(nums[i]) > nums.length / 2) {
-      return nums[i];
-    }
+  for (const num of nums) {
+    if (count === 0) candidate = num;
+    count += num === candidate ? 1 : -1;
   }
+
+  return candidate;
 };
